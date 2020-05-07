ea355cd92ee7c500d200e34740f4203034f1cd73
Viktorija <vktrjkndr@gmail.com>
2020-05-07 11:32:18 +0300

    Add second task

--- second_task/second_task.py
+++ second_task/second_task.py
@@ -1,1 +1,30 @@
+from pymongo import MongoClient
 
+
+client = MongoClient()
+database = client.database
+
+def first():
+    query = database.collection.find()
+    for record in query:
+        print(record)
+
+def second():
+    query = database.collection.find({},{'restaurant_id' : 1, 'name' : 1, 'borough': 1, 'cuisine': 1})
+    for record in query:
+        print(record)
+
+def third():
+    query = database.collection.find({},{"_id": 0,'restaurant_id' : 1, 'name' : 1, 'borough': 1, 'cuisine': 1})
+    for record in query:
+        print(record)
+
+def fourth():
+    query = database.collection.find({'borough': "Bronx"})
+    for record in query:
+        print(record)
+
+def fift():
+    query = database.collection.find({'grades': {'$elemMatch': {'score': {'$gte': 80, '$lte': 100 }}}})
+    for record in query:
+        print(record)
